6d0b7b2ebd71422838e926b700309261e174eb70
sumeet-at-scaler <sumeet@scaler.com>
2025-12-09 18:16:15 +0530

    ladskj

---
+++ 9thdec2025/remove_primes.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+
+public class remove_primes {
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 13, 17, 44, 47, 54));
+        System.out.println(list);
+        // removePrimes(list);
+        System.out.println(list);
+    }
+
+    
+}
